d23de708313e98be5bb9f4fbc5c61e161b255f09
jonseb974 <5151@holbertonstudents.com>
2023-04-01 14:24:07 +0200

    add code commentary

--- TypeScript/task_0/js/main.ts
+++ TypeScript/task_0/js/main.ts
@@ -5,25 +5,25 @@ interface Student {
     location: string;
 }
 
-const student1: Student = {
+const student1: Student = { // Variable student1
     firstName: 'Bruce',
     lastName: 'Wayne',
     age: 27,
     location: 'gotham city',
 };
 
-const student2: Student = {
+const student2: Student = { // Variable student2
     firstName: 'Clark',
     lastName: 'Kent',
     age: 30,
     location: 'Metropolis',
 };
 
-const studentList: Student[] = [student1, student2];
+const studentList: Student[] = [student1, student2]; // Store variables in array
 
-const table = document.createElement('table');
+const table = document.createElement('table'); // create a  table
 
-studentList.forEach((student) => {
+studentList.forEach((student) => { // Render a table
   const row = table.insertRow();
   const cell1 = row.insertCell();
   const cell2 = row.insertCell();
