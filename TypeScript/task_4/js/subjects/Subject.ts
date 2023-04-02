a0af23e04a2e8d1d7d6e99b27d2e500a2b868235
jonseb974 <5151@holbertonstudents.com>
2023-04-02 20:14:17 +0200

    delete file

---
+++ TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,14 @@
+namespace Subjects {
+  export class Subject {
+    private _teacher: Teacher;
+
+    setTeacher(teacher: Teacher): void {
+      this._teacher = teacher;
+    }
+
+    get teacher(): Teacher {
+      return this._teacher;
+    }
+  }
+}
+
