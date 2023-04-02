9089294c97e518d04db1d89160560039af74c5c8
jonseb974 <5151@holbertonstudents.com>
2023-04-02 20:17:28 +0200

    add file_Cpp

--- TypeScript/task_4/js/subjects/Cpp.ts
+++ TypeScript/task_4/js/subjects/Cpp.ts
@@ -1,9 +1,20 @@
 namespace Subjects {
-  export class Subject {
-    teacher: Teacher;
+  export interface Teacher {
+    experienceTeachingC?: number;
+  }
 
-    setTeacher(teacher: Teacher) {
-      this.teacher = teacher;
+  export class Cpp extends Subject {
+    getRequirements(): string {
+      return "Here is the list of requirements for Cpp";
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher && this.teacher.experienceTeachingC) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      } else {
+        return "No available teacher";
+      }
     }
   }
 }
+
