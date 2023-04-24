aea9e8ad59103f6a75d17aa7622a39fdd7e98667
jonseb974 <5151@holbertonstudents.com>
2023-04-24 17:24:09 +0200

    add dashboard_main

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -6,3 +6,10 @@ $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p');
+
+let count = 0;
+
+function updateCounter() {
+    count++;
+    $('#count').text(`${count} clicks on the button`);
+}
