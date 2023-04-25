ef34511a272d44b567581bbc881fdbeb8b587911
jonseb974 <5151@holbertonstudents.com>
2023-04-25 10:55:41 +0200

    update

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -1,11 +1,15 @@
 import $ from 'jquery';
 
+import $ from 'lodash'; // import _ from
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p');
+
+// debounce to the button
+$('button'.on)('click', _.debounce(updateCounter, 500));
 
 let count = 0;
 
