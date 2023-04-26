34902f72710543fdc655e9e0ccf3c14e040aea87
jonseb974 <5151@holbertonstudents.com>
2023-04-26 15:07:14 +0200

    add header.js

---
+++ Webpack/task_3/modules/header/header.js
@@ -0,0 +1,9 @@
+
+import $ from 'jquery';
+// styles
+import './header.css';
+
+
+$('body').append(`<div id="logo"></div>`);
+$('body').append(`<p>Holberton Dashboard</p>`);
+console.log('Init header');
