fce895964a4c03eec228e837ec53633941553b07
jonseb974 <5151@holbertonstudents.com>
2023-04-28 19:03:21 +0200

    add App.test.js

--- react_intro/task_3/dashboard/src/App.test.js
+++ react_intro/task_3/dashboard/src/App.test.js
@@ -1,37 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
-import {shallow } from 'enzyme';
 
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-//create 4 tests
-//test that App renders without crashing
-//verify that App renders a div with the class App-header
-//verify that App renders a div with the class App-body
-//verify that App renders a div with the class App-footer
 
 describe('<App />', () => {
-	it('Test that App renders without crashing', () => {
+	it('Tests that App renders without crashing', () => {
 		const wrapper = shallow(<App />);
 		expect(wrapper.exists()).toBe(true);
 	})
 
-	it('Test that App renders a di> with class "App-header"', () => {
+	it('Tests that App renders a <div> with class "App-header"', () => {
 		const wrapper = shallow(<App />);
 		expect(wrapper.find('.App-header').length).toBe(1);
 	})
 
-	it('Test that App renders a div with class "App-body"', () => {
+	it('Tests that App renders a <div> with class "App-body"', () => {
 		const wrapper = shallow(<App />);
 		expect(wrapper.find('.App-body').length).toBe(1);
 	})
 
-	it('Test that App renders a div with class "App-footer"', () => {
+	it('Tests that App renders a <div> with class "App-footer"', () => {
 		const wrapper = shallow(<App />);
 		expect(wrapper.find('.App-footer').length).toBe(1);
 	})
