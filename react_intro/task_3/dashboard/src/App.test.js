411cc873933c677104f689674261328cd9d49a3b
jonseb974 <5151@holbertonstudents.com>
2023-04-28 19:50:20 +0200

    add App.test.js

--- react_intro/task_3/dashboard/src/App.test.js
+++ react_intro/task_3/dashboard/src/App.test.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
 
 describe('App', () => {
   it('renders without crashing', () => {
