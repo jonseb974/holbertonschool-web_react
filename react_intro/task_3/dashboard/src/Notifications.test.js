c21d9fdee30691ff1fca7ff62acf414f561b2726
jonseb974 <5151@holbertonstudents.com>
2023-04-28 18:49:19 +0200

    add Notifications.test.js

--- react_intro/task_3/dashboard/src/Notifications.test.js
+++ react_intro/task_3/dashboard/src/Notifications.test.js
@@ -4,17 +4,17 @@ import Notifications from './Notifications';
 
 describe('<Notifications />', () => {
 	it('tests that Notifications renders without crashing', () => {
-		const wrapper = shallow(<Notifications />);
+		//const wrapper = shallow(<Notifications />);
 		expect(wrapper.exists()).toBe(true);
 	})
 
 	it('tests that Notifications renders three list items', () => {
-		const wrapper = shallow(<Notifications />);
+		//const wrapper = shallow(<Notifications />);
 		expect(wrapper.find('li').length).toBe(3);
 	})
 
 	it('tests that Notifications renders the text "Here is the list of notifications"', () => {
-		const wrapper = shallow(<Notifications />);
+		//const wrapper = shallow(<Notifications />);
 		expect(wrapper.find('p').text()).toBe('Here is the list of notifications');
 	})
 });
