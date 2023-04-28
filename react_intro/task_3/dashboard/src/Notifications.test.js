80e46def2d73fc1c5503345ad82ef065db44e18d
jonseb974 <5151@holbertonstudents.com>
2023-04-28 19:51:27 +0200

    add Notifications.test.js

--- react_intro/task_3/dashboard/src/Notifications.test.js
+++ react_intro/task_3/dashboard/src/Notifications.test.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Notifications from './Notifications';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
 
 describe('Notifications', () => {
   it('renders without crashing', () => {
