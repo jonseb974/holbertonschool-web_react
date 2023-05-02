aaef765e6fa5015ce34150fd3eafb1fe2525fc82
jonseb974 <5151@holbertonstudents.com>
2023-05-02 16:15:49 +0200

    add src/Login/App.js

--- react_props/task_1/dashboard/src/Login/App.js
+++ react_props/task_1/dashboard/src/Login/App.js
@@ -1,38 +1,24 @@
 import React, { Component } from 'react'
-import logo from './logo.jpg';
-import { getFullYear, getFooterCopy } from './utils'
-import Notifications from 'root-notifications'
-
-//import './App.css';
+import Notifications from '../Notifications/Notifications'
+import Login from '../Login/Login'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
 
 export default class App extends Component {
-  render() {
-    return (
-    <>
-    <div className='root-notifications'>
-            <div className="App">
-                <header className="Header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1>School dashboard</h1>
-                </header>
-                <body className='Login'>
-                    <p>Login to access the full dashboard</p>
-                    <label htmlFor="email" onClick={() => {
-                        document.getElementById('password').focus();
-                        }}>Email:  </label>
-                    <input type="email" id="email"/>
-                    <label htmlFor="password" onClick={() => {
-                        document.getElementById('password').focus();
-                        }}>  Password:  </label>
-                        <input type="password" id="password"/>
-                        <button>  OK </button>
-                </body>
-                <footer className='Footer'>
-                    <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
-                </footer>
-            </div>
-        </div>
-        </>
-    )
-  }
-}  
+	render() {
+		return (
+			<React.Fragment>
+				<Notifications />
+				<div className="App">
+					<Header />
+					<div className="App-body">
+						<Login />
+					</div>
+					<div className="App-footer">
+						<Footer />
+					</div>
+				</div>
+			</React.Fragment>
+		)
+	}
+}
