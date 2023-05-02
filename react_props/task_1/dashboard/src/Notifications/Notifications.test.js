fbb4f93f169376c4f92a86fb61da906cca5a3254
jonseb974 <5151@holbertonstudents.com>
2023-05-02 19:10:06 +0200

    update

--- react_props/task_1/dashboard/src/Notifications/Notifications.test.js
+++ react_props/task_1/dashboard/src/Notifications/Notifications.test.js
@@ -1,6 +1,6 @@
-
+import React from 'react';
 import { shallow } from 'enzyme';
-import Notifications from './Notifications';
+import Notifications from '../Notifications/Notifications';
 
 
 describe('<Notifications />', () => {
