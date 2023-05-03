28d5f016822235c23bfb37d5887347efadf0464f
jonseb974 <5151@holbertonstudents.com>
2023-05-03 15:54:45 +0200

    add Notifications.js

--- react_props/task_2/dashboard/src/Notifications/Notifications.js
+++ react_props/task_2/dashboard/src/Notifications/Notifications.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import close_icon from '../assets/close-icon.png'
 import { getLatestNotification } from '../utils/utils'
-
+import NotificationItem from './NotificationItem'
 import './Notifications.css'
 
 export default function Notification(props) {
@@ -21,9 +21,9 @@ export default function Notification(props) {
 	  </button>
 	  <p>Here is the list of notifications</p>
 	  <ul>
-		  <li data-priority="default">New course available</li>
-		  <li data-priority="urgent">New resume available</li>
-	  	  <li dangerouslySetInnerHTML={getLatestNotification()}></li>
+		  <NotificationItem type="default" value="New course available" />
+		  <NotificationItem type="urgent" value="New resume available" />
+	  	  <NotificationItem type="urgent" html={{ __html: getLatestNotification() }} />
 	  </ul>
 	</div>
   )
