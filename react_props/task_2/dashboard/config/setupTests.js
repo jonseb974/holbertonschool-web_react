657ccfd9c183bff119139ac8bc867a2c3387c77e
jonseb974 <5151@holbertonstudents.com>
2023-05-03 21:19:06 +0200

    add setupTests.js

--- react_props/task_2/dashboard/config/setupTests.js
+++ react_props/task_2/dashboard/config/setupTests.js
@@ -1,4 +1,9 @@
 import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import util from 'util';
+
+Object.defineProperty(global, 'TextEncoder', {
+  value: util.TextEncoder,
+});
 
 Enzyme.configure({ adapter: new Adapter() });
