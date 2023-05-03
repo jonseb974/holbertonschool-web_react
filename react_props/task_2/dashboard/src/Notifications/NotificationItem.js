b24884266a4754c9e62b78fabb51048794930b13
jonseb974 <5151@holbertonstudents.com>
2023-05-03 21:53:05 +0200

    add test

--- react_props/task_2/dashboard/src/Notifications/NotificationItem.js
+++ react_props/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotificationItem = ({ type, value, html }) => {
 	if (value) return(<li data-notification-type={type}>{value}</li>)
-	if (html) return(<li data-notification-type={type} dangerouslySetInnerHTML={html}></li>)
+	if (html) return(<li data-notification-type={type} dangerouslySetInnerHTML={html}>test</li>)
 	return(<li data-notification-type={type}></li>)
 };
 
--- react_props/task_2/dashboard/src/Notifications/NotificationItem.test.js
+++ react_props/task_2/dashboard/src/Notifications/NotificationItem.test.js
@@ -21,7 +21,7 @@ describe('<NotificationItem />', () => {
     })
 
     it('Passes `html` prop and checks for correct html rendering', () => {
-        const wrapper = shallow(<NotificationItem html={{ __html: 'dangerouslySetInnerHtml' }} />);
-        expect(wrapper.html()).toContain('dangerouslySetInnerHtml');
+        const wrapper = shallow(<NotificationItem html={{ __html: 'test' }} />);
+        expect(wrapper.html()).toContain('test');
     })
   })
