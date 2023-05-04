9e82232e4ca9ea1690ac9002915627dee14490fe
jonseb974 <5151@holbertonstudents.com>
2023-05-04 22:15:43 +0200

    update

--- react_props/task_0/dashboard/src/index.js
+++ react_props/task_0/dashboard/src/index.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom'; // 'react-dom/client';
-
-import './App.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App/App';
 
 
 createRoot(document.getElementById('root')).render(
   <>
-  <Notifications />
   <React.StrictMode>
     <App />
   </React.StrictMode>
   </>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
