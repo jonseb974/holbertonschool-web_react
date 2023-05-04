4e2850bee46539f88a23c99336217269e1b887ea
jonseb974 <5151@holbertonstudents.com>
2023-05-04 21:52:33 +0200

    update to src files

--- react_props/task_0/dashboard/src/index.js
+++ react_props/task_0/dashboard/src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom'; // 'react-dom/client';
+
+import './App.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Notifications from './Notifications';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
+    <div className="root-notifications">
+      <Notifications />
+    </div>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
