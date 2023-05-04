54e987c4b010bf08f8c47b6f9c4138c9858aea2f
jonseb974 <5151@holbertonstudents.com>
2023-05-04 22:41:33 +0200

    update to Header script

--- react_props/task_0/dashboard/src/Header/Header.js
+++ react_props/task_0/dashboard/src/Header/Header.js
@@ -1,37 +1,12 @@
-import React, { Component } from 'react'
-import logo from './logo.jpg';
-import { getFullYear, getFooterCopy } from './utils'
+import React from 'react';
+import logo from './assets/logo.jpg';
+import './Header.css';
 
-//import './App.css';
-
-export default class App extends Component {
-  render() {
-    return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>School dashboard</h1>
-      </header>
-      <body className='App-body'>
-        <p>Login to access the full dashboard</p>
-
-        <label htmlFor="email" onClick={() => {
-          document.getElementById('password').focus();
-        }}>Email:  </label>
-
-        <input type="email" id="email"/>
-
-        <label htmlFor="password" onClick={() => {
-          document.getElementById('password').focus();
-        }}>  Password:  </label>
-
-        <input type="password" id="password"/>
-        <button>  OK </button>
-      </body>
-      <footer className='App-footer'>
-        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
-      </footer>
-    </div>
-    )
-  }
+export default function Header(props) {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>School dashboard</h1>
+    </header>
+  );
 }  
