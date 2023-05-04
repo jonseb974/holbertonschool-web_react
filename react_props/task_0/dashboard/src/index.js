27255f096f4de705e5d9e2f78933695f33692db2
jonseb974 <5151@holbertonstudents.com>
2023-05-04 20:13:13 +0200

    update

--- react_props/task_0/dashboard/src/index.js
+++ react_props/task_0/dashboard/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom';
 
 import './App.css';
-import App from './App';
+import App from './App/App';
 import reportWebVitals from './reportWebVitals';
 
 
