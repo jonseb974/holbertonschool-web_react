990948ad39aa7f704cdc3dd28eef958ad0906f8c
jonseb974 <5151@holbertonstudents.com>
2023-05-04 21:24:16 +0200

    update

---
+++ react_props/task_0/dashboard/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import './App.css';
+import App from './App';
+
+
+createRoot(document.getElementById('root')).render(
+  <>
+    <div className="">
+      <Notifications />
+    </div>
+    <App />
+  </>
+);
+
