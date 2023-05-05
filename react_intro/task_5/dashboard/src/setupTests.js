09c8f1ae036718f1cf8b4ae95812d65c0f594e7e
jonseb974 <5151@holbertonstudents.com>
2023-05-05 15:04:09 +0200

    add setupTest file in src

---
+++ react_intro/task_5/dashboard/src/setupTests.js
@@ -0,0 +1,5 @@
+module.exports = {
+    roots: ["/home/bratak/holbertonschool-web_react/react_intro/task_5/dashboard/src"],
+    setupFiles: ["/home/bratak/holbertonschool-web_react/react_intro/task_5/dashboard/src/setupTests.js"],
+  };
+  
