44d9e2c59bfeda264e843882874fa2325c0b6228
jonseb974 <5151@holbertonstudents.com>
2023-05-05 16:43:28 +0200

    delete setupTest

--- react_intro/task_5/dashboard/src/setupTests.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    roots: ["/home/bratak/holbertonschool-web_react/react_intro/task_5/dashboard/src"],
-    setupFiles: ["/home/bratak/holbertonschool-web_react/react_intro/task_5/dashboard/src/setupTests.js"],
-  };
-  
