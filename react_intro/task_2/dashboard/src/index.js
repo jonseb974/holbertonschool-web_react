951a8cfa4cbf069284c78e8303fa705251cd0131
jonseb974 <5151@holbertonstudents.com>
2023-05-06 18:37:20 +0200

    update to index_js

--- react_intro/task_2/dashboard/src/index.js
+++ react_intro/task_2/dashboard/src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom'; // 'react-dom/client';
+import createRoot from 'react-dom/client';
 
 import './App.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Notifications from './Notifications';
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div className="root-notifications">
       <Notifications />
     </div>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>  
 );
 
 // If you want to start measuring performance in your app, pass a function
