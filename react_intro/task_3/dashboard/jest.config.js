f581f53b4e6f8abe8006045ff840d93def20d4d0
jonseb974 <5151@holbertonstudents.com>
2023-05-07 17:21:15 +0200

    add jest.config.js

---
+++ react_intro/task_3/dashboard/jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  transform: {
+    //"^.+\\.jsx?$": "babel-jest",
+    "^.+\\.css$": "jest-css-modules",
+    //"^.+\\.jpg$": "jest-transform-stub",
+    //"^.+\\.css$": "identity-obj-proxy"
+  },
+  testMatch: ["**/__tests__/**/*.js?(x)", "**/?(*.)+(spec|test).js?(x)"],
+  transform: {
+  //  "^.+\\.[jt]sx?$": "babel-jest",
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy"
+  },
+  transform: {
+    "^.+\\.jsx?$": "babel-jest"
+  }  
+};
+
