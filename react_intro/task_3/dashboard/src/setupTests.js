30cd7f2a742be1e52223d5e9b725913f86546449
jonseb974 <5151@holbertonstudents.com>
2023-05-09 15:42:09 +0200

    update

--- react_intro/task_3/dashboard/src/setupTests.js
+++ react_intro/task_3/dashboard/src/setupTests.js
@@ -10,4 +10,4 @@ import '@testing-library/jest-dom';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-Enzyme.configure({adapter: new Adapter()});
+Enzyme.configure({ adapter: new Adapter() });
