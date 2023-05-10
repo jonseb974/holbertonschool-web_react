c824d30f0b9660e13e569cb476d81e01da8272e9
jonseb974 <5151@holbertonstudents.com>
2023-05-10 20:17:57 +0200

    add Notifications and index

--- react_intro/task_5/dashboard/src/index.js
+++ react_intro/task_5/dashboard/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import './App.css';
 import App from './App';
-//import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals';
 import Notifications from './Notifications';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
