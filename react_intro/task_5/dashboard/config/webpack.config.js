2bb4e51147cc89d4db8b89131259b9602e6104c0
jonseb974 <5151@holbertonstudents.com>
2023-05-10 15:00:10 +0200

    add webpack.config

--- react_intro/task_5/dashboard/config/webpack.config.js
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,49 +1,34 @@
 const path = require('path');
 
-
 module.exports = {
-	mode: 'development',
-	entry: './src/index.js',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve('./dist')
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader']
-			},
-			{
-				test: /\.(png|jpe?g|gif)$/i,
-				use: [
-					'file-loader',
-					{
-						loader: 'image-webpack-loader',
-						options: {
-							bypassOnDebug: true,
-							disable: true
-						},
-					},
-				],
-			},
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env', '@babel/preset-react']
-					}
-				}
-			}
-		]
-	},
-	devtool: 'inline-source-map',
-	devServer: {
-		static: path.resolve('./dist'),
-		compress: true,
-		port: 8564,
-		hot: true,
-	},
-};
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "../dist")
+    },
+    devtool: "inline-source-map",
+    devServer: {
+        static: path.resolve(__dirname, "../dist"),
+        port: 8564,
+        hot: true,
+        open: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpe?g|gif)$/i,
+                type: 'asset/resource',
+                loader: 'image-webpack-loader',
+            },
+            {
+                test: /\.(jsx?)$/i,
+                exclude: /nodes_modules/,
+                loader: "babel-loader"
+            },
+        ],
+    },
+}
