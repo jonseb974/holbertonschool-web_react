bc96c821be500c7758e89a742c01a96740fe1459
jonseb974 <5151@holbertonstudents.com>
2023-05-10 23:22:41 +0200

    update setupfile

--- react_intro/task_5/dashboard/src/setupTests.js
+++ react_intro/task_5/dashboard/src/setupTests.js
@@ -4,6 +4,8 @@
 // learn more: https://github.com/testing-library/jest-dom
 
 import '@testing-library/jest-dom';
+import 'identity-obj-proxy';
+
 // setupTests.js
 //import Enzyme from 'enzyme';
 //import Adapter from '@wojtekmaj/enzyme-adapter-react-16';
@@ -11,3 +13,14 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
+
+const { defaults } = require('jest-config');
+
+module.exports = {
+  ...defaults,
+  transform: {
+    ...defaults.transform,
+    '^.+\\.css$': 'jest-transform-css',
+  },
+};
+
