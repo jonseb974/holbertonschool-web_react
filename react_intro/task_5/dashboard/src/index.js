3abc379874009ada1a2958424ad72b586e13b5cc
jonseb974 <5151@holbertonstudents.com>
2023-05-10 17:28:42 +0200

    update index_js file

---
+++ react_intro/task_5/dashboard/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './App.css';
+import App from './App';
+//import reportWebVitals from './reportWebVitals';
+import Notifications from './Notifications';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div className="root-notifications">
+      <Notifications />
+    </div>
+    <App />
+  </React.StrictMode>  
+);
