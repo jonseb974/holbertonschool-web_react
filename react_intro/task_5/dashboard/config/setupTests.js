b529c2d98c061bbe054ec16ac3843db75cf7dd8d
jonseb974 <5151@holbertonstudents.com>
2023-05-11 11:29:20 +0200

    update

--- react_intro/task_5/dashboard/config/setupTests.js
+++ react_intro/task_5/dashboard/config/setupTests.js
@@ -10,24 +10,24 @@
 //import Adapter from '@wojtekmaj/enzyme-adapter-react-16';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import 'identity-obj-proxy';
-import '@testing-library/jest-dom';
-import '@testing-library/jest-dom/extend-expect';
+//import 'identity-obj-proxy';
+//import '@testing-library/jest-dom';
+//import '@testing-library/jest-dom/extend-expect';
 
 
 Enzyme.configure({ adapter: new Adapter() });
 
-const { defaults } = require('jest-config');
+//const { defaults } = require('jest-config');
 
-module.exports = {
-  ...defaults,
-  transform: {
-    ...defaults.transform,
-    '^.+\\.css$': 'jest-transform-css',
-  },
-  moduleNameMapper: {
-    '\\.(css)$':'identity-obj-proxy',
-  },
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-};
+//module.exports = {
+//  ...defaults,
+//  transform: {
+//    ...defaults.transform,
+//    '^.+\\.css$': 'jest-transform-css',
+//  },
+//  moduleNameMapper: {
+//    '\\.(css)$':'identity-obj-proxy',
+//  },
+//  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+//};
 
