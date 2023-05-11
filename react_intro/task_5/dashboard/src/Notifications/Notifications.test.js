c75c42b948c3b02ce4afac0fd140cab7a409a885
jonseb974 <5151@holbertonstudents.com>
2023-05-11 10:45:29 +0200

    update the import statements

--- react_intro/task_5/dashboard/src/Notifications/Notifications.test.js
+++ react_intro/task_5/dashboard/src/Notifications/Notifications.test.js
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 //import Enzyme from 'enzyme';
 import Notifications from './Notifications';
 //import Adapter from 'enzyme-adapter-react-16';
+import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
 
 
