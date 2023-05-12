497fc2ddfe74f085a12f8ebc6e9ff97d7f99b738
jonseb974 <5151@holbertonstudents.com>
2023-05-12 18:07:33 +0200

    add setup files

---
+++ react_props/task_1/dashboard/src/App/App.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+
+
+describe('<App />', () => {
+	it('Tests that App renders without crashing', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.exists()).toBe(true);
+	})
+
+	it('Tests that App renders a <div> with class "App-header"', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find('.App-header').length).toBe(1);
+	})
+
+	it('Tests that App renders a <div> with class "App-body"', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find('.App-body').length).toBe(1);
+	})
+
+	it('Tests that App renders a <div> with class "App-footer"', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find('.App-footer').length).toBe(1);
+	})
+});
---
+++ react_props/task_1/dashboard/src/utils/utils.js
@@ -0,0 +1,17 @@
+function getFullYear() {
+      return new Date().getFullYear();
+}
+
+function getFooterCopy(isIndex) {
+    if (!isIndex) return "Holberton School main dashboard";
+    return "Holberton School";
+}
+
+function getLatestNotification() {
+      return {
+            __html: "<li data-priority='urgent'<strong>Urgent requirement</strong> - complete by EOD</li>"
+      }
+}
+
+// export
+export { getFullYear, getFooterCopy, getLatestNotification };
