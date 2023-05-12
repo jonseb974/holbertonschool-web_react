7660229e19229d6f3ae84b0b48602aeb2f9af7ae
jonseb974 <5151@holbertonstudents.com>
2023-05-12 10:03:47 +0200

    fixing syntax logic

--- react_intro/task_5/dashboard/src/index.js
+++ react_intro/task_5/dashboard/src/index.js
@@ -5,9 +5,16 @@ import './App/App.css';
 import App from './App/App';
 import Notifications from './Notifications/Notifications';
 
-const root = ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>  
+);
+
+const notifications = ReactDOM.createRoot(document.getElementById('root-notifications'));
+notifications.render(
   <React.StrictMode>
     <Notifications />
-    <App />
   </React.StrictMode>  
 );
