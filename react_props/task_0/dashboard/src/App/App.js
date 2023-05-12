906efce575c79001c49ba0f060e9b12f02776495
jonseb974 <5151@holbertonstudents.com>
2023-05-12 13:21:59 +0200

    add dashboard repository

---
+++ react_props/task_0/dashboard/src/App/App.js
@@ -0,0 +1,40 @@
+//import React, { Component } from 'react'
+import React from 'react';
+//import logo from './logo.jpg';
+import { getFullYear, getFooterCopy } from '../utils/utils'
+
+//import './App.css';
+
+function App () {
+    return (
+    <div className="App">
+      <header className="App-header">
+        <img src="" className="" alt="" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className='App-body'>
+        <p>Login to access the full dashboard</p>
+
+        <label htmlFor="email" onClick={() => {
+          document.getElementById('password').focus();
+        }}>Email:  </label>
+
+        <input type="email" id="email"/>
+
+        <label htmlFor="password" onClick={() => {
+          document.getElementById('password').focus();
+        }}>  Password:  </label>
+
+        <input type="password" id="password"/>
+        <button style={{ marginLeft: '10px'}}>  OK </button>
+      </body>
+      <footer className='App-footer'>
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+    </div>
+    );
+  }
+export default App;
+
+
+
