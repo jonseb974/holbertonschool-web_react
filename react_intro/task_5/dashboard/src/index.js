7e17a3983664a9106d9e53a92ddc3ce039931a77
jonseb974 <5151@holbertonstudents.com>
2023-05-12 09:52:31 +0200

    update to file id

--- react_intro/task_5/dashboard/src/index.js
+++ react_intro/task_5/dashboard/src/index.js
@@ -5,11 +5,9 @@ import './App/App.css';
 import App from './App/App';
 import Notifications from './Notifications/Notifications';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className="root">
-      <Notifications />
-    </div>
+    <Notifications />
     <App />
   </React.StrictMode>  
 );
