030f734803c22f84252baa7e6c6b92fd3483eafe
jonseb974 <5151@holbertonstudents.com>
2023-05-13 17:51:57 +0200

    add Notifications.js first item type to urget

--- react_props/task_3/dashboard/src/Notifications/Notifications.js
+++ react_props/task_3/dashboard/src/Notifications/Notifications.js
@@ -20,7 +20,7 @@ function Notifications() {
       >z</button>
       <p>Here is the list of notifications</p>
       <ul>
-        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New course available</li>
         <li data-priority="urgent">New resume available</li>
         <li data-priority="urgent"dangerouslySetInnerHTML={{__html: getLatestNotification()}}></li>
       </ul>
