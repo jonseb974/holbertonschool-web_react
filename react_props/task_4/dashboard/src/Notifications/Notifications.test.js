bd14a53d72226a003fbd6fe23a4a2dee7be012b2
jonseb974 <5151@holbertonstudents.com>
2023-05-14 19:03:48 +0200

    add Notifications.test.js

--- react_props/task_4/dashboard/src/Notifications/Notifications.test.js
+++ react_props/task_4/dashboard/src/Notifications/Notifications.test.js
@@ -2,26 +2,32 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Enzyme from 'enzyme';
 import Notifications from './Notifications';
-import Adapter from 'enzyme-adapter-react-16';
+import NotificationItem from './NotificationItem';
+//import Adapter from 'enzyme-adapter-react-16';
 //import '@testing-library/jest-dom';
 //import expect from 'expect';
 //import '@testing-library/jest-dom/extend-expect';
 
 
-Enzyme.configure({ adapter: new Adapter() })
+//Enzyme.configure({ adapter: new Adapter() })
 
 describe('<Notifications />', () => {
 	it('tests that Notifications renders without crashing', () => {
 		shallow(<Notifications />);
 	})
 
-	it('tests that Notifications renders three list items', () => {
-		const wrapper = shallow(<Notifications />);
-		expect(wrapper.find('li').length).toBe(3);
+	it('Passes dummy `type` prop and checks for correct HTML rendering', () => {
+		const wrapper = shallow(<NotificationItem type="default" value="test" />);
+		expect(wrapper.find('li').text()).toBe('test');
+	});
+
+	it('tests `value`that check renders HTML items', () => {
+		const wrapper = shallow(<Notifications type="default" value="test" />);
+		expect(wrapper.find('li').text()).toBe('test');
 	})
 
-	it('tests that Notifications renders the text "Here is the list of notifications"', () => {
-		const wrapper = shallow(<Notifications />);
-		expect(wrapper.find('p').text()).toBe('Here is the list of notifications');
+	it('tests `html` prop for correct HTML rendering', () => {
+		const wrapper = shallow(<NotificationItem html={{ __html: 'dangerouslySetInnerHTML' }} />);
+		expect(wrapper.html()).toContain('dangerouslySetInnerHTML');
 	})
 });
