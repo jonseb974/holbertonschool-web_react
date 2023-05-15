996c2f2891670eab54e65c2dacb6751348137e22
jonseb974 <5151@holbertonstudents.com>
2023-05-15 20:51:00 +0200

    update index script

--- react_props/task_4/dashboard/src/index.js
+++ react_props/task_4/dashboard/src/index.js
@@ -9,12 +9,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>  
-);
-
-const notifications = ReactDOM.createRoot(document.getElementById('root'));
-notifications.render(
-  <React.StrictMode>
     <Notifications />
   </React.StrictMode>  
 );
