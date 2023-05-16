244482f6afbbf65fe4c0ba8a56b1b8f4fccdf52b
jonseb974 <5151@holbertonstudents.com>
2023-05-16 13:37:55 +0200

    add Notifications_test

--- react_props/task_5/dashboard/src/Notifications/Notifications.test.js
+++ react_props/task_5/dashboard/src/Notifications/Notifications.test.js
@@ -1,28 +1,50 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Enzyme from 'enzyme';
-import Notifications from './Notifications';
-import NotificationItem from './NotificationItem';
-import PropTypes from 'prop-types';
+import React from 'react'
+import { shallow } from 'enzyme'
+import Enzyme from 'enzyme'
+import Notifications from './Notifications'
+import NotificationItem from './NotificationItem'
+import PropTypes from 'prop-types'
 
 
 describe('<Notifications />', () => {
 	it('tests that Notifications renders without crashing', () => {
-		shallow(<NotificationItem />);
-	})
-
-	it('Passes dummy `type` prop and checks for correct HTML rendering', () => {
-		const wrapper = shallow(<NotificationItem type="default" value="test" />);
-		expect(wrapper.find('li').text()).toBe('test');
+		const wrapper = shallow(<Notifications />);
+		expect(wrapper.exists()).toBe(true);
 	});
 
-	it('tests `value`that check renders HTML items', () => {
-		const wrapper = shallow(<NotificationItem type="default" value="test" />);
-		expect(wrapper.find('li').text()).toBe('test');
-	})
+	it('Check first Item renders correct html', () => {
+		const wrapper = shallow(<Notifications />);
+		expect(wrapper.find('p').first().text()).toEqual('Your notifications');
+	});
 
-	it('tests `html` prop for correct HTML rendering', () => {
-		const wrapper = shallow(<NotificationItem html={{ __html: 'dangerouslySetInnerHTML' }} />);
-		expect(wrapper.html()).toContain('dangerouslySetInnerHTML');
-	})
+	it('Tests menuItem rendered when displayDrawer is false', () => {
+		const wrapper = shallow(<Notifications displayDrawer={false} />);
+		expect(wrapper.find('.menuItem').exists()).toBe(true);
+	});
+
+	it('Tests the div Notifications is not rendered when displayDrawer is false', () => {
+		const wrapper = shallow(<Notifications displayDrawer={false} />);
+		expect(wrapper.find('.Notifications').exists()).toBe(false);
+	});
+	
+	it('Tests menuItem is rendered when displayDrawer is true', () => {
+		const wrapper = shallow(<Notifications displayDrawer listNotifications={[]} />);
+		expect(wrapper.find('.menuItem').exists()).toBe(true);
+	});
+
+	it('Tests the div Notifications is not rendered when displayDrawer is true', () => {
+		const wrapper = shallow(<Notifications displayDrawer listNotifications={[]} />);
+		wrapper.update();
+		expect(wrapper.find('.Notifications').exists()).toBe(true);
+	});
+
+	it('tests passing empty array', () => {
+		const wrapper = shallow(<Notifications listNotifications={[]} />);
+		expect(wrapper.find(NotificationItem).exist()).toBe(false);
+	});
+
+	it('tests when not passing listNotifications prop', () => {
+		const wrapper = shallow(<Notifications />);
+		expect(wrapper.find(NotificationItem).exists()).toBe(false);
+	});
 });
