e56a16a7a25ee784dace0e747e1dc9d23a912f60
jonseb974 <5151@holbertonstudents.com>
2023-05-16 13:58:55 +0200

    add CourseList

--- react_props/task_5/dashboard/src/CourseList/CourseList.test.js
+++ react_props/task_5/dashboard/src/CourseList/CourseList.test.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import CourseList from '../CourseList/CourseList';
-//import CourseListRow from '../CourseList/CourseListRow';
+//import CourseList from '../CourseList/CourseList';
+import CourseListRow from './CourseListRow';
 
-describe('<CourseList />', () => {
-  it(`Renders CourseList component without crashing`, () => {
-      const wrapper = shallow(<CourseList textFirstCell="Dummy Value" />);
-      expect(wrapper.exists()).toBe(true);
-  })
+describe('<CourseListRow />', () => {
+  it(`When isHeader is true,renders one cell with colSpan=2 when textSecondCell does not exist`, () => {
+      const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="test" />);
+      expect(wrapper.find('th').exists()).toBe(true);
+      expect(wrapper.find('th').prop('colSpan')).toBe(2);
+      expect(wrapper.find('th').texts()).toBe('test');
+  });
 
-  it(`Renders 5 CourseListRow components`, () => {
-      const wrapper = shallow(<CourseList />);
-      expect(wrapper.find('CourseListRow').length).toBe(5);
+  it(`When isHeader is true,renders one cells when textSecondCell exist`, () => {
+      const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="test" textSecondCell="test2" />);
+      expect(wrapper.find('th').length).toBe(2);
+      expect(wrapper.find('th').at(0).text()).toBe('test');
+      expect(wrapper.find('th').at(1).text()).toBe('test2');
+  });
+
+  it(`When isHeader is false,renders two td within a tr`, () => {
+      const wrapper = shallow(<CourseListRow isHeader={false} textFirstCell="test" textSecondCell="test2" />);
+      expect(wrapper.find('tr').length).toBe(1);
+      expect(wrapper.find('td').length).toBe(2);
+      expect(wrapper.find('td').at(0).text()).toBe('test');
+      expect(wrapper.find('td').at(1).text()).toBe('test2');
   });
 });
