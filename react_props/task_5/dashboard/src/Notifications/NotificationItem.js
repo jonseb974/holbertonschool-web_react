25f984864cee7d267835f893687e7bd261c0a455
jonseb974 <5151@holbertonstudents.com>
2023-05-16 09:40:19 +0200

    add dashboard

---
+++ react_props/task_5/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Notifications.css';
+import { getLatestNotification } from '../utils/utils';
+import PropTypes from 'prop-types';
+
+const NotificationItem = ({ type, html, value }) => {
+  const liProps = {
+    'data-notification-type': type,
+  };
+    
+    if (html) {
+      liProps.dangerouslySetInnerHTML= { __html: html };
+    }
+    return <li {...liProps}>{value}</li>;   
+  };
+  
+  NotificationItem.propTypes = {
+    type: PropTypes.string.isRequired,
+    html: PropTypes.string,
+    value: PropTypes.string.isRequired,
+  };
+  
+  export default NotificationItem;
