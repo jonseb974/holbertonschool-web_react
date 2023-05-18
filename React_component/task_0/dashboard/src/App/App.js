4169496e0a06b10c54ef321b0bebb4ae69d1b848
jonseb974 <5151@holbertonstudents.com>
2023-05-18 17:13:44 +0200

    add App

--- React_component/task_0/dashboard/src/App/App.js
+++ React_component/task_0/dashboard/src/App/App.js
@@ -6,23 +6,25 @@ import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import PropTypes from 'prop-types';
 import CourseList from '../CourseList/CourseList';
-import Notifications from '../Notifications/Notifications';
+//import Notifications from './Notifications/Notifications';
 
-
-const App = ({ isLoggedIn }) => {
+class App extends Component {
+  render() {
+    const { isLoggedIn } = this.props;
 
   const listCourses = [
     {id: 1, name: 'ES6', credit: 60},
     {id: 2, name: 'Webpack', credit: 20},
     {id: 3, name: 'React', credit: 40},
-  ]
+  ];
 
   const listNotifications = [
   {id: 1, type: 'default', value: 'New course available'},
   {id: 2, type: 'urgent', value: 'New resume available'},
   {id: 3,  type: 'urgent', html: {__html: getLatestNotification()}},
-]
-  return ( 
+];
+
+return (
   <div className="App">
     <Notifications displayDrawer={true} />
     <Header />
@@ -34,7 +36,8 @@ const App = ({ isLoggedIn }) => {
     </div>   
   </div>
   );
-};
+}
+}
 
 App.defaultProps = {
   isLoggedIn: false,
