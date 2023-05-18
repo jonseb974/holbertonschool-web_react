0a40327abd243af3bba72d39d6aad59744185c74
jonseb974 <5151@holbertonstudents.com>
2023-05-18 10:46:25 +0200

    update

--- react_props/task_5/dashboard/src/Notifications/NotificationItemShape.js
+++ react_props/task_5/dashboard/src/Notifications/NotificationItemShape.js
@@ -2,7 +2,8 @@ import propTypes from 'prop-types'
 
 const NotificationItemShape = propTypes.shape({
     id: propTypes.number.isRequired,
-    html: propTypes.objectOf(propTypes.string),
+    html: propTypes.shape({  __html: propTypes.string, }),
+    //html: propTypes.objectOf(propTypes.string),
     type: propTypes.string.isRequired,
     value: propTypes.string
 });
