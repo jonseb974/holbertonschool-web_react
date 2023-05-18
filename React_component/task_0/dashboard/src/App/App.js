0493825f3994131b8b95e26fca0e67e542f9741c
jonseb974 <5151@holbertonstudents.com>
2023-05-18 16:18:27 +0200

    add App_js

--- React_component/task_0/dashboard/src/App/App.js
+++ React_component/task_0/dashboard/src/App/App.js
@@ -1,53 +1,27 @@
 import React, { Component } from 'react'
 import Notifications from '../Notifications/Notifications';
-import { getLatestNotification } from '../utils/utils'
+import { getLatestNotification } from '../utils/utils';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import PropTypes from 'prop-types';
 import CourseList from '../CourseList/CourseList';
+import Notifications from '../Notifications/Notifications';
 
-
-const listCourses = [
-  {
-    id: 1,
-    name: 'ES6',
-    credit: 60,
-  },
-  {
-    id: 2,
-    name: 'Webpack',
-    credit: 20,
-  },
-  {
-    id: 3,
-    name: 'React',
-    credit: 40, 
-  }
-];
-
-
-const listNotifications = [
-  {
-    id: 1,
-    type: 'default',
-    value: 'New course available'
-  },
-  {
-    id: 2,
-    type: 'urgent',
-    value: 'New resume available'
-  },
-  {
-    id: 3,
-    html: {
-      __html: getLatestNotification()
-    },
-    type: 'urgent'
-  }
-];
 
 const App = ({ isLoggedIn }) => {
+
+  const listCourses = [
+    {id: 1, name: 'ES6', credit: 60},
+    {id: 2, name: 'Webpack', credit: 20},
+    {id: 3, name: 'React', credit: 40},
+  ]
+
+  const listNotifications = [
+  {id: 1, type: 'default', value: 'New course available'},
+  {id: 2, type: 'urgent', value: 'New resume available'},
+  {id: 3,  type: 'urgent', html: {__html: getLatestNotification()}},
+]
   return ( 
   <div className="App">
     <Notifications displayDrawer={true} />
