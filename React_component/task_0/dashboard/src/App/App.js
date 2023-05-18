65f2d3aba1f33f01f2b892b2bacbf0b6232e2ea1
jonseb974 <5151@holbertonstudents.com>
2023-05-18 15:58:54 +0200

    add a t_0 repo

---
+++ React_component/task_0/dashboard/src/App/App.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react'
+import Notifications from '../Notifications/Notifications';
+import { getLatestNotification } from '../utils/utils'
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import PropTypes from 'prop-types';
+import CourseList from '../CourseList/CourseList';
+
+
+const listCourses = [
+  {
+    id: 1,
+    name: 'ES6',
+    credit: 60,
+  },
+  {
+    id: 2,
+    name: 'Webpack',
+    credit: 20,
+  },
+  {
+    id: 3,
+    name: 'React',
+    credit: 40, 
+  }
+];
+
+
+const listNotifications = [
+  {
+    id: 1,
+    type: 'default',
+    value: 'New course available'
+  },
+  {
+    id: 2,
+    type: 'urgent',
+    value: 'New resume available'
+  },
+  {
+    id: 3,
+    html: {
+      __html: getLatestNotification()
+    },
+    type: 'urgent'
+  }
+];
+
+const App = ({ isLoggedIn }) => {
+  return ( 
+  <div className="App">
+    <Notifications displayDrawer={true} />
+    <Header />
+    <div className='App-body'>
+      {isLoggedIn ? <CourseList /> : <Login />}
+    </div>
+    <div className='App-footer'>
+      <Footer />
+    </div>   
+  </div>
+  );
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+};
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
+
+export default App;
---
+++ React_component/task_0/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Notifications.css';
+import { getLatestNotification } from '../utils/utils';
+import PropTypes from 'prop-types';
+
+const NotificationItem = ({ type, html, value }) => {
+  const liProps = {
+    'data-notification-type': type,
+  };
+    
+    if (html) {
+      liProps.dangerouslySetInnerHTML=  html;
+    }
+    return <li {...liProps}>{value}</li>;   
+  };
+  
+  NotificationItem.propTypes = {
+    type: PropTypes.string,
+    html: PropTypes.shape({ __html: PropTypes.string, }),
+    value: PropTypes.string.isRequired,
+  };
+
+  NotificationItem.defaultProps = {
+    type: 'default',
+  };
+
+export default NotificationItem;
