3d098b4e10e01aa8f72aa379b22bd0bacc7bb9a5
jonseb974 <5151@holbertonstudents.com>
2023-05-20 23:27:53 +0200

    update

--- React_component/task_1/dashboard/config/setupTests.js
+++ React_component/task_1/dashboard/config/setupTests.js
@@ -1,11 +1,10 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
+import { TextEncoder } from 'text-encoding';
 
 
 Enzyme.configure({ adapter: new Adapter() });
-
-//const { defaults } = require('jest-config');
+global.TextEncoder = TextEncoder;
 
 module.exports = {
   testEnvironment: 'jsdom',
