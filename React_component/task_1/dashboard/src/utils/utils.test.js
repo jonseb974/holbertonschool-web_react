0635a6e7129507ba5acef95fc9b0c0338ddddccd
jonseb974 <5151@holbertonstudents.com>
2023-05-21 15:48:30 +0200

    update

--- React_component/task_1/dashboard/src/utils/utils.test.js
+++ React_component/task_1/dashboard/src/utils/utils.test.js
@@ -1,28 +1,26 @@
-//import React from 'react';
-//import { act } from 'react-dom/test-utils';
 import { getFullYear, getFooterCopy, getLatestNotification } from './utils';
 
 
-// test to check getFullYear returns correct year
-test('getFullYear returns correct year', () => {
-	  expect(getFullYear()).toBe(2023);
-});
-
-
-// test to check that getFooter returns correct string
-// when arg is false
-test('getFooter returns correct string if false', () => {
-	  expect(getFooterCopy(false)).toBe('Holberton School main dashboard');
-});
-
-// test to check that getFooter returns correct string
-// when arg is true
-test('getFooter returns correct string if true', () => {
-	expect(getFooterCopy(true)).toBe('Holberton School');
-});
-
-// test to check that getLatestNotification returns correct
-// html string
-test('getLatestNotification returns correct html string', () => {
-	  expect(getLatestNotification().__html).toBe("<li data-priority='urgent'<strong>Urgent requirement</strong> - complete by EOD</li>");
+describe('Utils tests', () => {
+	// test to check getFullYear returns correct year
+	test('getFullYear returns correct year', () => {
+		expect(getFullYear()).toEqual(2023);
+	});
+	// test to check that getFooter returns correct string
+	// when arg is false
+	test('getFooter returns correct string if false', () => {
+		expect(getFooterCopy(false)).toEqual('Holberton School main dashboard');
+	});
+	
+	// test to check that getFooter returns correct string
+	// when arg is true
+	test('getFooter returns correct string if true', () => {
+		expect(getFooterCopy(true)).toEqual('Holberton School');
+	});
+	
+	// test to check that getLatestNotification returns correct
+	// html string
+	test('getLatestNotification returns correct html string', () => {
+		expect(getLatestNotification()).toEqual("<strong>Urgent requirement</strong> - complete by EOD");
+	});
 });
