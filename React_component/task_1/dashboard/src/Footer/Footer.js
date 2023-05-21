428b95aafd8d06014d792428746badccc485cace
jonseb974 <5151@holbertonstudents.com>
2023-05-21 15:22:07 +0200

    update

--- React_component/task_1/dashboard/src/Footer/Footer.js
+++ React_component/task_1/dashboard/src/Footer/Footer.js
@@ -1,37 +1,15 @@
-//import React, { Component } from 'react'
 import React from 'react';
-//import logo from './logo.jpg';
+import './Footer.css';
 import { getFullYear, getFooterCopy } from '../utils/utils'
 
-//import './App.css';
 
-function App () {
+const Footer = () => {
     return (
-    <div className="App">
-      <header className="App-header">
-        <img src="" className="" alt="" />
-        <h1>School dashboard</h1>
-      </header>
-      <body className='App-body'>
-        <p>Login to access the full dashboard</p>
-
-        <label htmlFor="email" onClick={() => {
-          document.getElementById('password').focus();
-        }}>Email:  </label>
-
-        <input type="email" id="email"/>
-
-        <label htmlFor="password" onClick={() => {
-          document.getElementById('password').focus();
-        }}>  Password:  </label>
-
-        <input type="password" id="password"/>
-        <button style={{ marginLeft: '10px'}}>  OK </button>
-      </body>
-      <footer className='App-footer'>
+      <div className={'App-footer'}>
         <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
-      </footer>
-    </div>
+      </div>
     );
-  }
-export default App;
+};
+
+
+export default Footer;
