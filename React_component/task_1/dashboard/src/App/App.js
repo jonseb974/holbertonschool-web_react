b7a2601f45afd01c7d6c00be4a3fcb1ad3a06543
jonseb974 <5151@holbertonstudents.com>
2023-05-21 14:55:11 +0200

    update

--- React_component/task_1/dashboard/src/App/App.js
+++ React_component/task_1/dashboard/src/App/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import Notifications from '../Notifications/Notifications';
-import { getLatestNotification } from '../utils/utils';
-import Login from '../Login/Login';
+
+import CourseList from '../CourseList/CourseList';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
+import Login from '../Login/Login';
 import PropTypes from 'prop-types';
-import CourseList from '../CourseList/CourseList';
-//import Notifications from './Notifications/Notifications';
+import { getLatestNotification } from '../utils/utils';
+import Notifications from './Notifications/Notifications';
 
 class App extends React.Component {
   constructor(props) {
@@ -36,29 +36,26 @@ class App extends React.Component {
       {id: 1, name: 'ES6', credit: 60},
       {id: 2, name: 'Webpack', credit: 20},
       {id: 3, name: 'React', credit: 40},
-    ];
+    ]
     
     const listNotifications = [
       {id: 1, type: 'default', value: 'New course available'},
       {id: 2, type: 'urgent', value: 'New resume available'},
       {id: 3,  type: 'urgent', html: {__html: getLatestNotification()}},
-    ];
-    
+    ]
     return (
-    <>
-    <div className="App">
-      <Notifications displayDrawer={true} />
-      <Header />
-      <div className='App-body'>
-        {isLoggedIn ? <CourseList /> : <Login />}
-      </div>
-      <div className='App-footer'>
-        <Footer />
-      </div>
-    </div>
-    </>
-  );
-}
+      <>
+        <Notifications displayDrawer={true} />
+        <div className={"App"}>
+          <Header />
+          <div className={'App-body'}>
+            {isLoggedIn ? <CourseList listCourses={listCourses}/> : <Login />}
+          </div>
+          <Footer />
+        </div>
+      </>
+    );
+  }
 }
 
 App.defaultProps = {
