cb5689e5a23a301af8fcefa924e01e115488b175
jonseb974 <5151@holbertonstudents.com>
2023-05-21 13:58:46 +0200

    update

--- React_component/task_1/dashboard/src/Login/Login.test.js
+++ React_component/task_1/dashboard/src/Login/Login.test.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Enzyme from 'enzyme';
 import Login from './Login';
-import Adapter from 'enzyme-adapter-react-16';
-//import '@testing-library/jest-dom';
-//import expect from 'expect';
-//import '@testing-library/jest-dom/extend-expect';
 
-
-Enzyme.configure({ adapter: new Adapter() })
 
 describe('<Login />', () => {
-	it('tests that Login renders without crashing', () => {
-		shallow(<Login />);
-	})
+	let wrapper;
 
-	it('tests that Login renders two checks', () => {
-		const wrapper = shallow(<Login />);
-		expect(wrapper.find('input')).toHaveLength(2);
-        expect(wrapper.find('label')).toHaveLength(2);
-	});
+	beforeEach(() => {
+		wrapper = shallow(<Login />);
+		
+		it('tests that Login renders without crashing', () => {
+			expect(wrapper.exists()).toBe(true);			
+		});
+	
+		it('tests that Login renders two checks', () => {
+			const wrapper = shallow(<Login />);
+			expect(wrapper.find('input')).toHaveLength(2);
+			expect(wrapper.find('label')).toHaveLength(2);
+		});
+	})
+	
 });
