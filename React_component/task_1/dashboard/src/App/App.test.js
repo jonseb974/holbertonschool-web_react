c89528362530e10d7ec3e84bde649388ee3f4c1f
jonseb974 <5151@holbertonstudents.com>
2023-05-22 12:45:20 +0200

    add test update

--- React_component/task_1/dashboard/src/App/App.test.js
+++ React_component/task_1/dashboard/src/App/App.test.js
@@ -1,65 +1,83 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from './App';
-import Notifications from '../Notifications/Notifications';
-import Header from '../Header/Header';
-import Login from '../Login/Login';
-import Footer from '../Footer/Footer';
-import CourseList from '../CourseList/CourseList';
-
 import PropTypes from 'prop-types';
 
+import App from './App';
+import CourseList from '../CourseList/CourseList';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Notifications from '../Notifications/Notifications';
+import { getLatestNotification } from '../utils/utils';
 
 
-describe('<App />', () => {
-	let wrapper;
+describe('App component tests', () => {
 
-	beforeEach(() => {
+	describe('When isLoggedIn = false', () => {
+  
+	  let wrapper;
+  
+	  beforeEach(() => {
 		wrapper = shallow(<App />);
-	});
-
-	it('Tests that App renders without crashing', () => {
+	  });
+  
+	  it('renders App without crashing', () => {
 		expect(wrapper.exists()).toBe(true);
-	})
-	
-	it('Tests that App renders a <div> with class "App-body"', () => {
-		expect(wrapper.find('.App-body').length).toBe(1);
-	})
-
-	it('Tests that App renders a <div> with class "App-footer"', () => {
-		expect(wrapper.find('.App-footer').length).toBe(1);
-	})
-	it('contain the Notifications component', () => {
-		expect(wrapper.contains(<Notifications displayDrawer={true} />)).toBe(true);
-	});
-	
-	it('contain the Header component', () => {
+	  });
+  
+	  it('renders a div with the class App-body', () => {
+		const appBody = wrapper.find('.App-body');
+		expect(appBody).toHaveLength(1);
+	  });
+  
+	  it('renders contain the Notifications component', () => {
+		const listNotifications = [
+		  {id: 1, type: "default", value: "New course available"},
+		  {id: 2, type: "urgent", value: "New resume available"},
+		  {id: 3, type: "urgent", html: {__html: getLatestNotification()}},
+		]
+		expect(wrapper.contains(<Notifications listNotifications={listNotifications}/>)).toBe(true);
+	  });
+  
+	  it('renders contain the Header component', () => {
 		expect(wrapper.contains(<Header />)).toBe(true);
-	});
-	
-	it('contain the Login component', () => {
+	  });
+  
+	  it('renders contain the Login component', () => {
 		expect(wrapper.contains(<Login />)).toBe(true);
-	});
-	
-	it('contain the Footer component', () => {
+	  });
+  
+	  it('renders contain the Footer component', () => {
 		expect(wrapper.contains(<Footer />)).toBe(true);
-	});
-
-	it('does not contain the CourseList component when isLoggedIn is false', () => {
+	  });
+  
+	  it('renders not contain the CourseList component', () => {
 		expect(wrapper.contains(<CourseList />)).toBe(false);
+	  });
+	})
+  
+	describe('When isLoggedIn = true', () => {
+  
+	  it('renders the component CourseList whereas Login', () => {
+		const listCourses = [
+		  {id: 1, name: "ES6", credit: 60},
+		  {id: 2, name: "Webpack", credit: 20},
+		  {id: 3, name: "React", credit: 40},
+		]
+		const wrapper = shallow(<App isLoggedIn={true}/>);
+		expect(wrapper.contains(<CourseList listCourses={listCourses}/>)).toBe(true);
+		expect(wrapper.contains(<Login />)).toBe(false);
+	  });
+  
+	  it('calls logOut function when keys Control and H are pressed', () => {
+		const logOutMock = jest.fn();
+		shallow(<App logOut={logOutMock} />);
+  
+		const alert = jest.spyOn(global, 'alert');
+		expect(alert);
+		expect(logOutMock);
+  
+		jest.restoreAllMocks();
+	  });
 	});
-	
-	describe('when isLoggedIn is true', () => {
-		beforeEach(() => {
-			wrapper.setProps({ isLoggedIn: true });
-		});
-		
-		it('does not contain the Login component', () => {
-			expect(wrapper.contains(<Login />)).toBe(false);
-		});
-		
-		it('contains the CourseList component', () => {
-			expect(wrapper.contains(<CourseList />)).toBe(true);
-		});
-	});
-});
+  });
