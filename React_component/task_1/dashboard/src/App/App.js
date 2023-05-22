aebf2deee0c496f58bb8ac7689166af2a64bee4a
jonseb974 <5151@holbertonstudents.com>
2023-05-22 16:36:08 +0200

    add App_js

--- React_component/task_1/dashboard/src/App/App.js
+++ React_component/task_1/dashboard/src/App/App.js
@@ -9,22 +9,41 @@ import CourseList from '../CourseList/CourseList';
 //import Notifications from './Notifications/Notifications';
 
 class App extends React.Component {
+  handleKeyDown = (event) => {
+    const { logOut } = this.props;
+    if (event.ctrlKey && event.key === 'h') {
+      // Display 
+      alert('Logging you out');
+      logOut();
+    }
+  };
+
+  componentDidMount() {
+    // Add event listener when component is mounted
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    // Remove event listener when component is unmounted
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
+  
   render() {
     const { isLoggedIn } = this.props;
 
   const listCourses = [
-    {id: 1, name: 'ES6', credit: 60},
-    {id: 2, name: 'Webpack', credit: 20},
-    {id: 3, name: 'React', credit: 40},
+    { id: 1, name: 'ES6', credit: 60 },
+    { id: 2, name: 'Webpack', credit: 20 },
+    { id: 3, name: 'React', credit: 40 },
   ];
 
   const listNotifications = [
-  {id: 1, type: 'default', value: 'New course available'},
-  {id: 2, type: 'urgent', value: 'New resume available'},
-  {id: 3,  type: 'urgent', html: {__html: getLatestNotification()}},
-];
-
-return (
+    { id: 1, type: 'default', value: 'New course available' },
+    { id: 2, type: 'urgent', value: 'New resume available' },
+    { id: 3,  type: 'urgent', html: {__html: getLatestNotification() }},
+  ];
+  
+  return (
   <div className="App">
     <Notifications displayDrawer={true} />
     <Header />
