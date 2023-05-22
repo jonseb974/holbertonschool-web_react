1e7014338b38529bd25e72446068b58613583259
jonseb974 <5151@holbertonstudents.com>
2023-05-22 13:16:03 +0200

    add dashboard

---
+++ React_component/task_1/dashboard/src/Login/Login.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Enzyme from 'enzyme';
+import Login from './Login';
+import Adapter from 'enzyme-adapter-react-16';
+//import '@testing-library/jest-dom';
+//import expect from 'expect';
+//import '@testing-library/jest-dom/extend-expect';
+
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('<Login />', () => {
+	it('tests that Login renders without crashing', () => {
+		shallow(<Login />);
+	})
+
+	it('tests that Login renders two checks', () => {
+		const wrapper = shallow(<Login />);
+		expect(wrapper.find('input')).toHaveLength(2);
+        expect(wrapper.find('label')).toHaveLength(2);
+	});
+});
---
+++ React_component/task_1/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import NotificationItem from './NotificationItem';
+import NotificationItemShape from './NotificationItemShape';
+
+import './Notifications.css';
+
+
+const Notifications = ({ displayDrawer, listNotifications }) => {
+  return (
+  <>
+    <div className="menuItem">
+      <p>Your notifications</p>
+    </div>
+    {displayDrawer && (
+    <div className="Notifications">
+      <button style={{
+        position: "absolute",
+        background: "transparent",
+        border: "none",
+        right: "20px",
+        cursor:'pointer'
+      }}
+      aria-label="close"
+      onClick= {() => {
+        console.log('Close button has been clicked\n');
+      }}
+      >x</button>
+      {listNotifications.length === 0 ? <p>No new notification for now </p>
+        : <p>Here is the list of notifications</p>}
+      <ul>
+        {listNotifications.map((notification) => (
+            <NotificationItem
+            key={notification.id}
+            id={notification.id}
+            html={notification.html} 
+            type={notification.type}
+            value={notification.value} 
+            />
+          ))}
+      </ul>
+    </div>
+    )}
+    </>
+  );
+};
+
+Notifications.defaultProps = { 
+  displayDrawer: false,
+  listNotifications: [],
+}
+
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
+  //displayDrawer: PropTypes.bool.isRequired,
+  //0listNotifications: PropTypes.arrayOf(PropTypes.string).isRequired
+  //listNotifications: PropTypes.arrayOf(PropTypes.string),
+  //listNotifications: PropTypes.arrayOf(NotificationItem.propTypes),
+  listNotifications: PropTypes.arrayOf(NotificationItemShape),
+};
+
+export default Notifications;
