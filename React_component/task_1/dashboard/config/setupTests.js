7c6e8662d0db5b45df65b48377bd3604b5e67f12
jonseb974 <5151@holbertonstudents.com>
2023-05-22 17:38:59 +0200

    update setup

--- React_component/task_1/dashboard/config/setupTests.js
+++ React_component/task_1/dashboard/config/setupTests.js
@@ -4,14 +4,9 @@ import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-const shallow  = Enzyme.shallow;
-const mount = Enzyme.mount;
-
 module.exports = {
   moduleNameMapper: {
     '\\.(css)$': '<rootDir>/__mocks__/styleMock.js',
     "\\.(jpg)$": "<rootDir>/__mocks__/fileMock.js"
   },
 };
-
-export { shallow, mount };
