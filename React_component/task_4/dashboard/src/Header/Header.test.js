3e75b24398f5694704f9df4becffca94f93566b6
jonseb974 <5151@holbertonstudents.com>
2023-05-22 20:48:46 +0200

    add dashboard

---
+++ React_component/task_4/dashboard/src/Header/Header.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Enzyme from 'enzyme';
+import Header from './Header';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('<Header />', () => {
+	it('tests that Header renders without crashing', () => {
+		shallow(<Header />);
+	})
+
+	it('tests that Header renders three list items', () => {
+		const wrapper = shallow(<Header />);
+		expect(wrapper.find('img')).toHaveLength(1);
+        expect(wrapper.find('img')).toHaveLength(1);
+	});
+});
