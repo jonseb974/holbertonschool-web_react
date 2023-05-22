c2cbd3c86fe8ffa9de2fbe50f54550f4e23aca8f
jonseb974 <5151@holbertonstudents.com>
2023-05-22 17:34:51 +0200

    add setup

--- React_component/task_1/dashboard/config/setupTests.js
+++ React_component/task_1/dashboard/config/setupTests.js
@@ -1,23 +1,11 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-
-
-
-// setupTests.js
-//import Enzyme from 'enzyme';
-//import Adapter from '@wojtekmaj/enzyme-adapter-react-16';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-//import 'identity-obj-proxy';
-//import '@testing-library/jest-dom';
-//import '@testing-library/jest-dom/extend-expect';
 
 
 Enzyme.configure({ adapter: new Adapter() });
 
-//const { defaults } = require('jest-config');
+const shallow  = Enzyme.shallow;
+const mount = Enzyme.mount;
 
 module.exports = {
   moduleNameMapper: {
@@ -26,3 +14,4 @@ module.exports = {
   },
 };
 
+export { shallow, mount };
