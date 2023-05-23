6e70531d4dbcfa78e3e8a35ffe87cfa6790c585e
jonseb974 <5151@holbertonstudents.com>
2023-05-23 20:35:57 +0200

    updatePureComponent

--- React_component/task_5/dashboard/src/Notifications/NotificationItem.js
+++ React_component/task_5/dashboard/src/Notifications/NotificationItem.js
@@ -1,29 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import './Notifications.css';
-//import { getLatestNotification } from '../utils/utils';
+class NotificationItem extends React.PureComponent {
+  render() {
+    const { type, html, value, markAsRead, id } = this.props;
+    let li;
 
+    value
+      ? (li = (
+          <li data-notification-type={type} onClick={() => markAsRead(id)}>
+            {value}
+          </li>
+        ))
+      : (li = (
+          <li
+            data-notification-type={type}
+            dangerouslySetInnerHTML={html}
+            onClick={() => markAsRead(id)}
+          ></li>
+        ));
 
-const NotificationItem = ({ type, html, value, markAsRead }) => {
-  const liProps = {
-    'data-notification-type': type,
-    onClick: () => markAsRead(id),
-  };
-  
-  if (html) liProps.dangerouslySetInnerHTML=  html;
-  return <li {...liProps}>{value}</li>;   
-  };
-  
-  NotificationItem.propTypes = {
-    type: PropTypes.string,
-    html: PropTypes.shape({ __html: PropTypes.string, }),
-    value: PropTypes.string,
-    markAsRead: PropTypes.func.isRequired,
-  };
+    return li;
+  }
+}
 
-  NotificationItem.defaultProps = {
-    type: 'default',
-  };
+NotificationItem.defaultProps = {
+  type: 'default',
+  html: {},
+  value: '',
+  markAsRead: () => {},
+  id: NaN,
+};
+
+NotificationItem.propTypes = {
+  type: PropTypes.string,
+  html: PropTypes.shape({
+    __html: PropTypes.string,
+  }),
+  value: PropTypes.string,
+  markAsRead: PropTypes.func,
+  id: PropTypes.number,
+};
 
 export default NotificationItem;
