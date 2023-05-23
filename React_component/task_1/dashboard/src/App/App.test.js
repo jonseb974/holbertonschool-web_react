6584c1678ff7f3ff7fd3128dedd2a1387daef7f1
jonseb974 <5151@holbertonstudents.com>
2023-05-23 11:49:39 +0200

    import update

--- React_component/task_1/dashboard/src/App/App.test.js
+++ React_component/task_1/dashboard/src/App/App.test.js
@@ -8,8 +8,6 @@ import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
 import CourseList from '../CourseList/CourseList';
 
-import PropTypes from 'prop-types';
-
 
 
 describe('<App />', () => {
