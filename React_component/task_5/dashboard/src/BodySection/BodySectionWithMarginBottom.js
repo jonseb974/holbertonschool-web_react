d812317816173d486fe6bad7d7df620e8492ddf1
jonseb974 <5151@holbertonstudents.com>
2023-05-24 11:09:33 +0200

    update the file

--- React_component/task_5/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ React_component/task_5/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,31 +1,23 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
 import BodySection from './BodySection';
-import './BodySectionWithMarginBottom.css'; // import styling section
+
+import './BodySection.css'; 
 
 
-class BodySectionWithMarginBottom extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    
+class BodySectionWithMarginBottom extends React.Component {    
     render() {
         return (
             <div className="bodySectionWithMargin">
                 <BodySection {...props}/>           
             </div>
         );
-    }
+    };
 }   
 
 BodySectionWithMarginBottom.PropTypes = {
-    title: PropTypes.string.isRequired,
-    children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]), 
-};
-
-BodySectionWithMarginBottom.defaultProps = {
-    children: <React.Fragment />,
-};
+    title: PropTypes.string,
+    children: PropTypes.node,
+}
 
 export default BodySectionWithMarginBottom;
