d7f766577149979eff660fb8d1e87f00e2c651f4
jonseb974 <5151@holbertonstudents.com>
2023-05-24 10:10:19 +0200

    update index import

--- React_component/task_5/dashboard/src/index.js
+++ React_component/task_5/dashboard/src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Notifications from './Notifications/Notifications'; 
-import './App/App.css';
 import App from './App/App';
 
 
