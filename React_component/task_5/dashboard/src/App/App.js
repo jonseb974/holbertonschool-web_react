e26f52f1448180fe61806330fee72e19e9b26f25
jonseb974 <5151@holbertonstudents.com>
2023-05-24 10:58:20 +0200

    update the file

--- React_component/task_5/dashboard/src/App/App.js
+++ React_component/task_5/dashboard/src/App/App.js
@@ -1,12 +1,17 @@
-import React, { Component } from 'react'
-import Notifications from '../Notifications/Notifications';
-import { getLatestNotification } from '../utils/utils';
-import Login from '../Login/Login';
+import React from 'react';
+
+import './App.css';
+import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
+import BodySection from "../BodySection/BodySection";
+import CourseList from '../CourseList/CourseList';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
+import Notifications from '../Notifications/Notifications';
+import Login from '../Login/Login';
 import PropTypes from 'prop-types';
-import CourseList from '../CourseList/CourseList';
-//import Notifications from './Notifications/Notifications';
+import { getLatestNotification } from '../utils/utils';
+
+
 
 class App extends React.Component {
   constructor(props) {
@@ -38,36 +43,44 @@ class App extends React.Component {
       { id: 1, name: 'ES6', credit: 60 },
       { id: 2, name: 'Webpack', credit: 20 },
       { id: 3, name: 'React', credit: 40 }
-    ];
+    ]
     
     const listNotifications = [
       { id: 1, type: 'default', value: 'New course available' },
       { id: 2, type: 'urgent', value: 'New resume available' },
       { id: 3,  type: 'urgent', html: {__html: getLatestNotification() }},
-    ];
+    ]
+
     const { isLoggedIn } = this.props;
     
     return (
       <>
-        <Notifications displayDrawer={true} />
-        <Header />
-        {isLoggedIn ? ( 
-          <BodySectionWithMarginBottom title='Course list'>
-            <CourseList listCourses={listCourses} />
-          </BodySectionWithMarginBottom>
-        ) : (
-          <BodySectionWithMarginBottom title='Log in to continue'>
-            <Login />
-          </BodySectionWithMarginBottom>
-        )}
-        <BodySection title='News from the School'>
-          <p>Paragraph with some random text.</p>
-        </BodySection>
-    
-        <Footer />
+        <Notifications listNotifications={listNotifications} />
+        <div className={'App'}>
+          <Header />
+          <div className={'App-body'}>
+            {
+            isLoggedIn ?
+            (
+            <BodySectionWithMarginBottom title={'Course list'}>
+              <CourseList listCourses={listCourses} />
+            </BodySectionWithMarginBottom>
+            ) :
+            (
+            <BodySectionWithMarginBottom title={'Log in to continue'}>
+              <Login />
+            </BodySectionWithMarginBottom>
+            )
+            }
+            <BodySection title={'News from the School'}>
+              <p>Paragraph with some random text.</p>
+            </BodySection>
+          </div>
+            <Footer />
+        </div>
       </>
     );
-  }
+  };
 }
 
 App.defaultProps = {
