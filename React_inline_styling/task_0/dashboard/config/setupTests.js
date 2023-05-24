58e309f19aa58809f89b6598adc3da1e07450649
jonseb974 <5151@holbertonstudents.com>
2023-05-24 23:10:42 +0200

    udate setup

--- React_inline_styling/task_0/dashboard/config/setupTests.js
+++ React_inline_styling/task_0/dashboard/config/setupTests.js
@@ -1,12 +1,6 @@
-import Enzyme from 'enzyme';
+import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 
-Enzyme.configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
 
-module.exports = {
-  moduleNameMapper: {
-    '\\.(css)$': '<rootDir>/__mocks__/styleMock.js',
-    "\\.(jpg)$": "<rootDir>/__mocks__/fileMock.js"
-  },
-};
