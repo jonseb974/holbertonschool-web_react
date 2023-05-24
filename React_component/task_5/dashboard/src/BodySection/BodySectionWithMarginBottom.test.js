740644fee59a4df97ce419ab8a17a8567ccb81e2
jonseb974 <5151@holbertonstudents.com>
2023-05-24 11:15:08 +0200

    add tests_js

--- React_component/task_5/dashboard/src/BodySection/BodySectionWithMarginBottom.test.js
+++ React_component/task_5/dashboard/src/BodySection/BodySectionWithMarginBottom.test.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import BodySectionWithMarginBottom from './BodySectionWithMarginBottom';
+import BodySection from "./BodySection";
+import {expectedError} from "@babel/core/lib/errors/rewrite-stack-trace";
 
-describe('<BodySectionWithMarginBottom />', () => {
-  it('renders component and props', () => {
-    const wrapper = shallow(
-      <BodySectionWithMarginBottom title='test title'>
-        <p>test children node</p>
-      </BodySectionWithMarginBottom>
-    );
-    const div = wrapper.find('.bodySectionWithMargin').first();
-    const BodySection = wrapper.find('BodySection');
-    const internalBody = BodySection.dive();
-    const h2 = internalBody.find('h2');
-    const p = internalBody.find('p');
+describe('BodySectionWithMarginBottom component tests', () => {
 
-    expect(div.exists()).toEqual(true);
-    expect(BodySection).toHaveLength(1);
-    expect(BodySection.props().title).toEqual('test title');
-    expect(h2).toHaveLength(1);
-    expect(h2.text()).toEqual('test title');
-    expect(p).toHaveLength(1);
-    expect(p.text()).toEqual('test children node');
+  let wrapper;
+  const props = {
+      title: "test title",
+      children: React.createElement('p', 'test children node'),
+  }
+
+  beforeEach(() => {
+      wrapper = shallow(<BodySection { ...props }/>);
+  });
+
+  it('renders the BodySection component with the props', () => {
+      expect(wrapper.containsAllMatchingElements([
+          <div className="BodySectionWithMargin">
+              <BodySection { ... props }/>
+          </div>
+      ]));
   });
 });
