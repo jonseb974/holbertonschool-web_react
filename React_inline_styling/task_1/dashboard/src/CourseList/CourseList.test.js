2c367b93af18b60a4bec9fc0bac9800988a7dda4
jonseb974 <5151@holbertonstudents.com>
2023-05-25 21:30:19 +0200

    add test StyleSheet

--- React_inline_styling/task_1/dashboard/src/CourseList/CourseList.test.js
+++ React_inline_styling/task_1/dashboard/src/CourseList/CourseList.test.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { StyleSheetTestUtils } from 'aphrodite';
 import CourseList from './CourseList';
 
 describe('CourseList component tests', () => {
+    beforeEach(() => {
+        StyleSheetTestUtils.suppressStyleInjection();
+    });
 
     describe('When listCourse is not empty', () => {
         let wrapper;
