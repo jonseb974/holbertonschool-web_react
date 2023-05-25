dca04019210ac3d4ba5f792494230b77e9696e90
jonseb974 <5151@holbertonstudents.com>
2023-05-25 08:47:45 +0200

    add CourseListRow

---
+++ React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,33 @@
+import './CourseList.css'
+
+const CourseListRow = ({isHeader, textFirstCell, textSecondCell}) => {
+    let tr;
+
+    if (isHeader && textSecondCell === null) tr = <th colSpan={2}>{textFirstCell}</th>;
+    else if (isHeader && textSecondCell) tr = (
+        <React.Fragment>
+            <th>{textFirstCell}</th>
+            <th>{textSecondCell}</th>
+        </React.Fragment>
+    ); else tr = (
+        <React.Fragment>
+            <td>{textFirstCell}</td>
+            <td>{textSecondCell}</td>
+        </React.Fragment>
+    );
+
+    return <tr>{tr}</tr>;
+};
+
+CourseListRow.propTypes = {
+    isHeader: PropTypes.bool,
+    textFirstCell: PropTypes.string.isRequired,
+    textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
+
+CourseListRow.defaultProps = {
+    isHeader: false,
+    textSecondCell: null,
+};
+
+export default CourseListRow;
