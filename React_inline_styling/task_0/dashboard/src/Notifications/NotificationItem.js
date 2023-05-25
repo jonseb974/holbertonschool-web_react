d21b41f9889e01f7c2a290e9117091dac42f1635
jonseb974 <5151@holbertonstudents.com>
2023-05-25 09:08:57 +0200

    add js file

---
+++ React_inline_styling/task_0/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,29 @@
+class NotificationsItem extends React.PureComponent {
+    render() {
+        const { type, html, value, markAsRead, id } = this.props;
+        const liProps = {
+            'data-notification-type': type,
+        };
+        if (html) liProps.dangerouslySetInnerHTML = html;
+        return (
+            <li {...liProps} onClick={() => (markAsRead(id))}>{value}</li>
+        );
+    }
+}
+
+NotificationsItem.propTypes = {
+    id: PropTypes.number,
+    html: PropTypes.shape({
+        __html: PropTypes.string,
+    }),
+    type: PropTypes.string,
+    value: PropTypes.string,
+    markAsRead: PropTypes.func,
+};
+
+NotificationsItem.defaultProps = {
+    type: 'default',
+    markAsRead: () => {},
+};
+
+export default NotificationsItem;
