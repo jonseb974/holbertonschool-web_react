1b6f4e74c71d11202768f113884968a0856571b2
jonseb974 <5151@holbertonstudents.com>
2023-05-25 14:26:31 +0200

    add js file

--- React_inline_styling/task_1/dashboard/src/Login/Login.js
+++ React_inline_styling/task_1/dashboard/src/Login/Login.js
@@ -1,19 +1,51 @@
 import React from 'react';
-import './Login.css';
+import { StyleSheet } from 'aphrodite';
+
+
+const styles = StyleSheet.create({
+    loginParagraph: {
+        margin: '3rem 0 0 2rem',
+    },
+    appForm: {
+        display: 'flex',
+        margin: '1rem 0 0 2rem',
+    },
+    formLabel: {
+        margin: '0 1rem 0 0',
+    },
+    formInput: {
+        margin: '0 1rem 0 0',
+    },
+});
 
 const Login = () => {
     return (
         <React.Fragment>
-            <p className={"login-p"}>Login to access the full dashboard</p>
-            <div className={"App-form"}>
-                <label htmlFor={"email"}>Email:</label>
-                <input type={"email"} id={"email"} name={"email"}></input>
-                <label htmlFor={"password"}>Password:</label>
-                <input type={"password"} id={"password"} name={"password"}></input>
-                <button type={"submit"}>OK</button>
+            <p className={css(styles.loginParagraph)}>
+                Login to access the full dashboard</p>
+            <div className={css(styles.appForm)}>
+                <label htmlFor="email" className={css(styles.formLabel)}>
+                Email:
+                </label>
+                <input
+                    type={"email"} 
+                    id={"email"} 
+                    name={"email"}
+                    className={css(styles.formInput)}>
+                    </input>
+                <label htmlFor="password" className={css(styles.formLabel)}>
+                    Password:
+                </label>
+                <input 
+                    type={"password"}
+                    id={"password"}
+                    name={"password"}
+                    className={css(styles.formInput)}>
+                    </input>
+                <button type="submit">OK</button>
             </div>
         </React.Fragment>
     );
-}
+};
 
 export default Login;
