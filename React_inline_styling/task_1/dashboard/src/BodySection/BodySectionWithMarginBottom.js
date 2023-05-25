30a1bc60ebb61dbef320db7dd0552b27c2ffa607
jonseb974 <5151@holbertonstudents.com>
2023-05-25 11:59:57 +0200

    update styles in file

--- React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
 import BodySection from './BodySection';
-import './BodySection.css';
+
+
+const styles = StyleSheet.create({
+    bodySectionWithMargin: {
+      marginBottom: '40px',
+    },
+});
 
 class BodySectionWithMarginBottom extends React.Component {
     render() {
         return (
-            <div className={"bodySectionWithMargin"}>
+            <div className={css(styles.bodySectionWithMargin)}>
                 <BodySection {...this.props} />
             </div>
         );
