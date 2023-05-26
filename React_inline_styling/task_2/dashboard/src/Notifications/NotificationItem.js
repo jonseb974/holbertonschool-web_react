c8cafdf7cedf83a8071d5207b1db2b76ab99b086
jonseb974 <5151@holbertonstudents.com>
2023-05-26 10:32:13 +0200

    add js file

--- React_inline_styling/task_2/dashboard/src/Notifications/NotificationItem.js
+++ React_inline_styling/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -3,29 +3,20 @@ import PropTypes from 'prop-types';
 import { StyleSheet, css } from 'aphrodite';
 
 
+const styles = StyleSheet.create({
+    defaultItem: {
+
+    },
+    urgentItem: {
+
+    },
+});
 class NotificationsItem extends React.PureComponent {
     render() {
-        const styles =  StyleSheet.create({
-        
-            .Notifications {
-                border: 2px dotted var(--holberton-red);
-                padding: 1rem .5rem 0 .5rem;
-                float: right;
-            }
-            li[data-notification-type="default"] {
-                color: darkblue;
-            }
-            li[data-notification-type="urgent"] {
-                color: red;
-            }
-            .menuItem {
-                text-align: rig
-            }
-        });
-
         const { type, html, value, markAsRead, id } = this.props;
         const liProps = {
             'data-notification-type': type,
+            className: css(type === 'urgent' ? styles.urgentItem :  styles.defaultItem),
         };
         if (html) liProps.dangerouslySetInnerHTML = html;
         return (
