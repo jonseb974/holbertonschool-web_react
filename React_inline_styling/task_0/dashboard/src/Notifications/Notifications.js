6d9d2dca2668b18e42ebcabfbfd8224ad5c8cbcc
jonseb974 <5151@holbertonstudents.com>
2023-05-27 18:11:33 +0200

    add import

--- React_inline_styling/task_0/dashboard/src/Notifications/Notifications.js
+++ React_inline_styling/task_0/dashboard/src/Notifications/Notifications.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './Notifications.css';
+import Notifications from './Notifications.css';
 import NotificationsItem from './NotificationsItem';
 import NotificationItemShape from './NotificationItemShape';
 
