92d2df8f95c2c94e3b56566725df3541d3afcd3a
jonseb974 <5151@holbertonstudents.com>
2023-05-29 14:15:09 +0200

    add Header_test_js

---
+++ react_state/task_0/dashboard/src/Header/Header.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Header from './Header';
+
+describe('Header component tests', () => {
+
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<Header />);
+    });
+
+    it('renders Header without crashing', () => {
+        expect(wrapper.exists()).toBe(true);
+    });
+
+    it('renders a div with the class App-header', () => {
+        const appHeader = wrapper.find('.App-header');
+        expect(appHeader).toHaveLength(1);
+    });
+
+    it('renders a tag img', () => {
+        const appHeader = wrapper.find('img');
+        expect(appHeader).toHaveLength(1);
+    });
+
+    it('renders a tag h1', () => {
+        const appHeader = wrapper.find('h1');
+        expect(appHeader).toHaveLength(1);
+    });
+});
