da5b61e39cf15a6187ff81611cc39b9468b4c3bd
jonseb974 <5151@holbertonstudents.com>
2023-05-30 20:48:42 +0200

    add App_js with update

--- react_state/task_0/dashboard/src/App/App.js
+++ react_state/task_0/dashboard/src/App/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
 import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+import {getLatestNotification} from '../utils/utils';
+
 import './App.css';
 import Notifications from '../Notifications/Notifications';
 import Header from '../Header/Header';
 import Login from '../Login/Login';
 import CourseList from '../CourseList/CourseList';
 import Footer from '../Footer/Footer';
-import {getLatestNotification} from '../utils/utils';
 import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
 import BodySection from "../BodySection/BodySection";
 
