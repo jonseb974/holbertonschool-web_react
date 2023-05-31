960a08b8adf65bf99f1a452eaaf698b920846a75
jonseb974 <5151@holbertonstudents.com>
2023-05-31 08:15:34 +0200

    add Notiffications_js

--- react_state/task_0/dashboard/src/Notifications/Notiffications.js
+++ react_state/task_0/dashboard/src/Notifications/Notiffications.js
@@ -15,19 +15,21 @@ class Notifications extends React.Component {
     }
 
     shouldComponentUpdate(nextProps, nextState, nextContext) {
-        return nextProps.listNotifications.length > this.props.listNotifications.length;
+        return (nextProps.listNotifications.length > this.props.listNotifications.length ||
+            nextProps.displayDrawer !== this.props.displayDrawer);
     }
 
     render() {
-        const { displayDrawer, listNotifications } = this.props;
+        const { displayDrawer, listNotifications, handleDisplayDrawer, handleHideDrawer } = this.props;
         return (
-            <React.Fragment>
-                <div className={"menuItem"}>
-                    <p>Your notifications</p>
+            <>
+                <div className={css(styles.menuItem)} onClick={handleDisplayDrawer}>
+                    <p className={css(styles.animationOpacity, styles.animationBounce)}>Your notifications</p>
                 </div>
                 {displayDrawer && (
-                    <div className={'Notifications'}>
+                    <div className={css(styles.notifications, styles.notificationsMobile)}>
                         <button
+                        className={css(styles.buttonMobile)}
                             style={{
                                 position: "absolute",
                                 right: "1rem",
@@ -57,7 +59,7 @@ class Notifications extends React.Component {
                         </ul>
                     </div>
                 )}
-            </React.Fragment>
+            </>
         );
     };
 }
@@ -65,11 +67,88 @@ class Notifications extends React.Component {
 Notifications.propTypes = {
     displayDrawer: PropTypes.bool,
     listNotifications: PropTypes.arrayOf(NotificationItemShape),
+    handleDisplayDrawer: PropTypes.func,
+    handleHideDrawer: PropTypes.func,
 };
 
 Notifications.defaultProps = {
     displayDrawer: false,
     listNotifications: [],
+    handleDisplayDrawer: () => {},
+    handleHideDrawer: () => {},
 };
 
+const opacity = {
+    from: {
+        opacity: '.5',
+    },
+    to: {
+        opacity: '1',
+    },
+};
+
+const bounce = {
+    '70%': {
+        transform: 'translateY(0px)',
+    },
+    '85%': {
+        transform: 'translateY(-5px)',
+    },
+    '100%': {
+        transform: 'translateY(5px)',
+    }
+};
+
+const styles = StyleSheet.create({
+    notifications: {
+        border: '2px dotted #e1484c',
+        padding: '1rem .5rem 0 .5rem',
+        position: 'fixed',
+        right: '0',
+        top: '0',
+        backgroundColor: 'rgba(255, 248, 248, 1)',
+    },
+    menuItem: {
+        textAlign: 'right',
+        ':hover': {
+            cursor: 'pointer'
+        }
+    },
+    notificationsMobile: {
+        '@media (max-width: 900px)': {
+            fontSize: '20px',
+            position: 'fixed',
+            top: '0',
+            bottom: '0',
+            left:'0',
+            right: '0',
+            zIndex: '9999',
+            backgroundColor: 'rgba(255, 255, 255, 1)',
+        },
+    },
+    buttonMobile: {
+        '@media (max-width: 900px)': {
+            top: '1rem',
+        },
+    },
+    ulMobile: {
+        '@media (max-width: 900px)': {
+            padding: '0 !important',
+        },
+    },
+    animationOpacity: {
+        ':hover': {
+            animationName: opacity,
+            animationDuration: '1s',
+            animationIterationCount: '3',
+        },
+    },
+    animationBounce: {
+        ':hover': {
+            animationName: bounce,
+            animationDuration: '0.5s',
+            animationIterationCount: '3',
+        },
+    },
+});
 export default Notifications;
